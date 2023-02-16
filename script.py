ef444d0d636da576940a7622f440280194e32c84
HayHay <dev@hayhay.dev>
2023-02-16 01:23:12 -0500

    Finish script, export all .paper to .md locally

--- script.py
+++ script.py
@@ -3,10 +3,10 @@ import dropbox
 from pathlib import Path
 
 # Dropbox access token
-ACCESS_TOKEN = 'sl.BY5mqRa4uiXya7tMB4GZhqbvrE-npa78bKUBA5oCww6xf2xLWUoMoS7MpBQCqbqFf63T9ek_n_6WPzzDGJIM0wnhTkZRxyU0fsMYS8DNeU84STzJiKAKkhvU9ovUjKtCnnAexey-U_T0'
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
 
 # Create a Dropbox object
-dbx = dropbox.Dropbox(ACCESS_TOKEN)
+dbx: dropbox.Dropbox = dropbox.Dropbox(ACCESS_TOKEN)
 
 # Folder path in Dropbox Paper
 PAPER_PATH = ''
@@ -17,32 +17,12 @@ DOWNLOAD_PATH = './downloaded'
 # Create folder to save downloaded files
 Path(DOWNLOAD_PATH).mkdir(parents=True, exist_ok=True)
 
-# List all files in the root folder of Dropbox
-# for entry in dbx.files_list_folder(PAPER_PATH).entries:
 for entry in dbx.files_search(PAPER_PATH, '.paper').matches:
-    print(entry.metadata, "\n\n")
     entry = entry.metadata
-    md, res = dbx.files_export(entry.path_lower)
-
-    with open(os.path.join(DOWNLOAD_PATH, entry.name.replace(".paper", ".html")), 'wb') as f:
-        f.write(res.content)
+    dropbox_path = os.path.dirname(f"./downloaded{entry.path_lower}")
+    if dropbox_path:
+        dropbox_path = f"{dropbox_path}"
+        Path(dropbox_path).mkdir(parents=True, exist_ok=True)
     
-
-# print(f'Downloaded and converted {entry.name} to Markdown format')
-
-
-
-    # if isinstance(entry, dropbox.files.FileMetadata):
-    #     print(entry.metadata)
-    #     if entry.name.endswith('.paper'):
-    #         md, res = dbx.files_download(entry.path_lower)
-        
-    #         with open(os.path.join(DOWNLOAD_PATH, entry.name), 'wb') as f:
-    #             f.write(res.content)
-
-            
-    #         # Convert the file to Markdown format
-    #         dbx.files_download_to_file(os.path.join(DOWNLOAD_PATH, entry.name + '.md'), entry.path_lower, rev=None)
-
-    #         print(f'Downloaded and converted {entry.name} to Markdown format')
-
+    dbx.files_export_to_file(os.path.join(dropbox_path, entry.name.replace(".paper", ".md")), entry.path_lower, "markdown")
+    print(f'Downloaded and converted {entry.name} to Markdown format')
